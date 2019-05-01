ea46c8cb7b888c4865cac0c72cc8f87a49396ce8
Lou1sM <oneillml@tcd.ie>
2019-05-01 19:33:36 +0100

    merge the two utils.py files

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,40 @@
 from __future__ import print_function
 import os
 import json
+import math
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+import matplotlib.ticker as ticker
+import numpy as np
+import time
 
+
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+# Function computing: 
+# - time elapsed 
+# - estimated time remaining given the current time and progress %
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
+
+
+
+def showPlot(points):
+    plt.figure()
+    fig, ax = plt.subplots()
+    # this locator puts ticks at regular intervals
+    loc = ticker.MultipleLocator(base=0.2)
+    ax.yaxis.set_major_locator(loc)
+    plt.plot(points)
+    plt.savefig('loss.png')
 def read_json_file(input_json):
 	## load the json file
 	file_info = json.load(open(input_json, 'rb'))
