daeceeb6767dd043ef61011f2970f2b8b959c2a5
Lou1sM <oneillml@tcd.ie>
2019-05-02 14:39:54 +0100

    replace multiple get_framerate functions with a single one, split videos into train/val/test and loop through each to create separate file for each; add shortcut arg of 'jade' for frame_dir

--- src/data_prep.py
+++ src/data_prep.py
@@ -11,7 +11,8 @@ input paramters:
     --num_test: number of test images (to withold until very very end)
 
 example command: 
-    p data_prep.py --frame_dir data/frames --input_json data/YT_40_structured_captions.json  --output_jso   n some_file_name.json --output_h5 some_file_name.h5
+    p data_prep.py --frame_dir data/frames --input_json data/YT_40_structured_captions.json  --output_json
+    some_file_name.json --output_h5 some_file_name.h5
 """
 
 from __future__ import print_function
@@ -27,9 +28,7 @@ from skimage import img_as_float
 
 
 def split_data(vids, params):
-    """
-     This function just split the data to val/test/train
-    """
+    """Split the data into val/test/train."""
     count_val = 0
     count_test = 0
     count_train = 0
@@ -65,31 +64,14 @@ def split_data(vids, params):
     else:
         print ("%d assigned to test, %d to val, and %d to train" % (count_test, count_val, count_train) )
 
-def get_framrate_msrvtt(sample_video, params):
-    """
-     return the frame rate of MSR_VTT
-     this function is MSR_VTT specific
-    """
+
+def get_framerate(sample_video, params):
+    """Return the framerate of a video."""
     return np.shape(np.load(params.frame_dir+'/'+sample_video['video_id']+params.v_f_name_ext)['arr_0'])[0]
 
-def get_framrate_HW(sample_video, params):
-    """
-     return the frame rate of Hollywood
-     this function is Hollywood specific
-    """
-    return np.shape(np.load(params.frame_dir+'/'+sample_video['video_id']+params.v_f_name_ext)['arr_0'])[0]
-
-def get_framrate_YT(sample_video, params):
-    """
-     return the frame rate of YT
-     this function is YT specific
-    """
-    return np.shape(np.load(params.frame_dir+'/'+sample_video['video_id']+params.v_f_name_ext)['arr_0'])[0]
 
 def resize_video(params, frame_rate, input_vid):
-    """
-    Need to loop over video frames to resize it
-    """
+    """Loop over video frames to resize them."""
     num_channel = 3 # thsi one should be a parameters
     resized_vid = np.zeros((frame_rate, num_channel, params.img_size, params.img_size))
 
@@ -99,9 +81,9 @@ def resize_video(params, frame_rate, input_vid):
 
     return resized_vid
 
+
 def main(params):
     dtype = 'uint32'
-    #dtype_vid = 'uint8' # this should be a parameter
     dtype_vid = 'float64' # this should be a parameter
     num_channel = 3     # this should be calculated from input data
 
@@ -111,62 +93,69 @@ def main(params):
     split_data(videos, params)
     dict_to_json = {}
     dict_to_json['video'] = []
-    h5_f = h5py.File(params.output_h5, 'w')
 
+    train_videos = [v for v in videos if v['split'] == 'train']
+    val_videos = [v for v in videos if v['split'] == 'val']
+    test_videos = [v for v in videos if v['split'] == 'test']
+    data_dict = {'train': train_videos, 'val': val_videos, 'test': test_videos}
+    print('\nData split is:')
+    for data_split, videos in data_dict.items():
+        print(data_split+': ',len(videos))
     # Rename the key to fit with Daniel's code
-    for v in videos:
-        v['video_id'] = v['gn']
+    for data_split, videos in data_dict.items():
+        for v in videos:
+            v['video_id'] = v['gn']
+        print(videos[0].keys())
+            
+        if params.local:
+            # Just for testing, when running locally
+            videos = [v for v in videos if v['video_id'] in ["vid1", "vid2", "vid3", "vid4"]]
+            if len(videos) == 0:
+                continue
+          ##Since we are dealing with videos, we need to get numebr of frame per video
+        frame_rate = get_framerate(videos[np.random.randint(len(videos))], params)
+
+        N = len(videos)
+        print("\nProcessing the {} data. {} videos in this split".format(data_split, N))
+        out_h5_file_name = data_split + '_' + params.output_h5
+        out_json_file_name = data_split + '_' + params.output_json
+        if params.local:
+            out_h5_file_name = 'local_' + out_h5_file_name
+            out_json_file_name = 'local_' + out_json_file_name
+        h5_f = h5py.File(out_h5_file_name, 'w')
+        vid_data = h5_f.create_dataset("videos", (N,frame_rate, num_channel, params.img_size, params.img_size), dtype = dtype_vid)
         
-    if params.local:
-        # Just for testing, when running locally
-        videos = [v for v in videos if v['video_id'] in ["vid1", "vid2", "vid3", "vid4"]]
-    print(len(videos))
-      ##Since we are dealing with videos, we need to get numebr of frame per video
-    #Each video has the following format : 16*w*H*channel
-    if (params.dataset_name == 'msr_vtt'):
-        print(videos[np.random.randint(len(videos))].keys())
-        frame_rate = get_framrate_msrvtt(videos[np.random.randint(len(videos))], params)
-    elif ('HW'.lower() in params.dataset_name.lower() ):
-        frame_rate = get_framrate_HW(videos[np.random.randint(len(videos))], params)
-    elif ('YT'.lower() in params.dataset_name.lower() ):
-        frame_rate = get_framrate_YT(videos[np.random.randint(len(videos))], params)
-    else:
-        raise ValueError("No support for %s dataset yet" % params.dataset_name)
+        dummy_seq_lens = np.random.randint(low=2, high=9, size=(N))
+        dummy_embeddings = np.ones(shape=(N,10,300))
+        print(dummy_seq_lens)
+        print(dummy_embeddings.shape)
+        embedding_data = h5_f.create_dataset("embeddings", data=dummy_embeddings)
+        len_data = h5_f.create_dataset("seq_len", data=dummy_seq_lens)
+        
+        for idx, vid in enumerate(videos):
+            i_file = params.frame_dir+'/'+vid['video_id']+params.v_f_name_ext
+            assert os.path.exists(i_file) , "The %s file is not there, something is worng" % (i_file)
 
-    N = len(videos)
-    vid_data = h5_f.create_dataset("videos", (N,frame_rate, num_channel, params.img_size, params.img_size), dtype = dtype_vid)
-    
-    dummy_seq_lens = np.random.randint(low=2, high=9, size=(N))
-    dummy_embeddings = np.ones(shape=(N,10,300))
-    print(dummy_seq_lens)
-    print(dummy_embeddings.shape)
-    embedding_data = h5_f.create_dataset("embeddings", data=dummy_embeddings)
-    len_data = h5_f.create_dataset("seq_len", data=dummy_seq_lens)
-    
-    for idx, vid in enumerate(videos):
-        i_file = params.frame_dir+'/'+vid['video_id']+params.v_f_name_ext
-        assert os.path.exists(i_file) , "The %s file is not there, something is worng" % (i_file)
+            try:
+                input_vid = np.load(i_file)['arr_0']
+            except Exception:
+                print("Loading zip file failed; defaulted to zero vector.")
+                input_vid = np.zeros((frame_rate, num_channel, params.img_size, params.img_size))
+                pass
+            int_img = resize_video(params, frame_rate, input_vid)
+            d = np.array([[1,2],[3,4]])
+            float_img = img_as_float(np.ndarray.astype(int_img, dtype=np.uint8))
+            vid_data[idx] = float_img
+            # show some progress
+            if idx % 15 == 0:
+                print("%d out of %d have been processed" % (idx + 1, len(videos)))
 
-        try:
-            input_vid = np.load(i_file)['arr_0']
-        except Exception:
-            print("Loading zip file failed; defaulted to zero vector.")
-            input_vid = np.zeros((frame_rate, num_channel, params.img_size, params.img_size))
-            pass
-        int_img = resize_video(params, frame_rate, input_vid)
-        d = np.array([[1,2],[3,4]])
-        float_img = img_as_float(np.ndarray.astype(int_img, dtype=np.uint8))
-        vid_data[idx] = float_img
-        # show some progress
-        if idx % 15 == 0:
-            print("%d out of %d have been processed" % (idx + 1, len(videos)))
-
-        # add data to a dict to be dumped into a json final
-        dict_to_json['video'].append(vid)
+            # add data to a dict to be dumped into a json final
+            dict_to_json['video'].append(vid)
 
     h5_f.close()
     print("Done with H5 file")
-    json.dump(dict_to_json, open(params.output_json, 'w'))
+    json.dump(dict_to_json, open(out_json_file_name, 'w'))
     print("Done with everything")
 
 if __name__ == "__main__":
@@ -176,7 +165,7 @@ if __name__ == "__main__":
     # input json
     parser.add_argument('--input_json', required=True, help='input json file to process into hdf5')
     parser.add_argument('--limit', default=0, type=int, help='max number of vids to prepare')
-    parser.add_argument('--local', action='store_true', default=False, help='whether running locally, if so use just one data point')
+    parser.add_argument('--local', action='store_true', default=False, help='whether running locally, if so use just small number of data points')
     parser.add_argument('--output_json', default='dummy.json', help='output json file')
     parser.add_argument('--output_h5', default='dummy.h5', help='output h5 file')
     parser.add_argument('--old_json', default='', help='old json file to extend')
@@ -189,9 +178,10 @@ if __name__ == "__main__":
     parser.add_argument('--encoding', default='utf-8')
     parser.add_argument('--img_size', default=256, type =int, help = 'Each frame will be resize to this size')
     parser.add_argument('--v_f_name_ext', default='_f.npz', help = 'the format and extension of saved video name, e.g. video10_f.npz')
-    parser.add_argument('--dataset_name', default='msr_vtt', help = 'name of input dataset, only support msr_vtt as of now')
 
     args = parser.parse_args()
+    if args.frame_dir == 'jade':
+        args.frame_dir = '/jmain01/home/JAD015/ttl03/dxv38-ttl03/videocap/data/Youtube_frames_8'
     print('\nParsed input parameters:')
     print(json.dumps(vars(args), indent = 2))
     main(args)
