3143694d55c65e21d6d9876edd0353b632b6b8de
Lou1sM <oneillml@tcd.ie>
2019-05-04 13:41:36 +0100

    renamed seq2seq.py to models.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import utils
 import options
-import seq2seq
+import models
 import torch
 from data_loader import load_data
 
@@ -10,9 +10,9 @@ def main():
     exp_name = utils.get_datetime_stamp()
     print(args)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    encoder = seq2seq.EncoderRNN(args, device).to(device)
-    decoder = seq2seq.DecoderRNN(args, device).to(device)
-    regressor = seq2seq.NumIndRegressor(args,device).to(device)
+    encoder = models.EncoderRNN(args, device).to(device)
+    decoder = models.DecoderRNN(args, device).to(device)
+    regressor = models.NumIndRegressor(args,device).to(device)
 
     h5_train_generator = load_data(args.h5_file_path, args.batch_size, shuffle=args.shuffle)
     h5_val_generator = load_data(args.h5_file_path, args.batch_size, shuffle=args.shuffle)
@@ -26,9 +26,9 @@ def main():
         print(regressor)
 
     if args.model == 'seq2seq':
-        seq2seq.train_iters_seq2seq(args, encoder, decoder, train_generator=h5_train_generator, val_generator=h5_val_generator, print_every=1, plot_every=1, exp_name=exp_name)
+        models.train_iters_seq2seq(args, encoder, decoder, train_generator=h5_train_generator, val_generator=h5_val_generator, print_every=1, plot_every=1, exp_name=exp_name)
     elif args.model == 'reg':
-        seq2seq.train_iters_reg(args, encoder, regressor, train_generator=h5_train_generator, val_generator=h5_val_generator, print_every=1, plot_every=1, exp_name=exp_name)
+        models.train_iters_reg(args, encoder, regressor, train_generator=h5_train_generator, val_generator=h5_val_generator, print_every=1, plot_every=1, exp_name=exp_name)
     
 
 
