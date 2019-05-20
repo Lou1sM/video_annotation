7092cc05e42b4038fe970088ba7e260846561159
Lou1sM <oneillml@tcd.ie>
2019-05-20 15:48:51 +0100

    replace npy with npz files and include all frames

--- src/convert_img_to_normed_float.py
+++ src/convert_img_to_normed_float.py
@@ -3,10 +3,11 @@ from skimage import img_as_float
 import matplotlib.pyplot as plt
 import os
 
-for filename in os.listdir('../data/frames/'):
-    filepath = os.path.join('../data/frames', filename)
-    float_img = img_as_float(np.ndarray.astype(np.load(filepath)['arr_0'][0].transpose(1,2,0), dtype=np.uint8))
-    #plt.imshow(float_img)
+#for filename in os.listdir('../data/frames/'):
+for i in range(1,1971):
+    filepath = os.path.join('../data/frames', 'vid{}_resized.npz'.format(i))
+    float_img = img_as_float(np.ndarray.astype(np.load(filepath)['arr_0'].transpose(0,2,3,1), dtype=np.uint8))
+    #plt.imshow(float_img[0])
     #plt.show()
-    print(filename)
-    np.save(os.path.join('../data/norm_frames', filename), float_img)
+    print(i)
+    np.savez(os.path.join('../data/norm_frames', 'vid{}.npz'.format(i)), float_img)
