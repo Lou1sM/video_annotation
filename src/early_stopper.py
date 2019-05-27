4508345d851e6f30b4e5a8682c8383c777002c2b
Lou1sM <oneillml@tcd.ie>
2019-05-27 09:52:33 +0100

    make early stopper print checkpoint file path

--- src/early_stopper.py
+++ src/early_stopper.py
@@ -32,7 +32,7 @@ class EarlyStopper:
     def save_checkpoint(self, val_loss, model_dict, filename='../checkpoints/chkpt.pt'):
         '''Saves model when validation loss decrease.'''
         if self.verbose:
-            print('Validation loss decreased ({} --> {}).  Saving model ...'.format(self.val_loss_min, val_loss))
+            print('Validation loss decreased ({} --> {}).  Saving model to {} ...'.format(self.val_loss_min, val_loss, filename))
         torch.save(model_dict,filename)
         self.val_loss_min = val_loss
         
