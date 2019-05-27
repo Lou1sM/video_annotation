a594bac8424dcb9014c3dbbc0c1a9c04a342b980
Lou1sM <oneillml@tcd.ie>
2019-05-27 09:55:16 +0100

    print l2 distance when making output.txt

--- src/get_output.py
+++ src/get_output.py
@@ -1,3 +1,4 @@
+import torch.nn as nn
 import os
 import json
 import sys
@@ -78,25 +79,45 @@ def get_output_gen(checkpoint_path, test_dataset, mode='seq2seq', device='cuda')
         encoder_outputs, encoder_hidden = encoder(input_tensor, encoder_hidden)
 
         print("real number of embeddings in video: ", int(target_number.item()))
-        decoder_input = torch.zeros(1, 1, decoder.hidden_size, device=decoder.device).to(device)
+        decoder_input = torch.zeros(1, 1, 50, device=decoder.device).to(device)
         if encoder.num_layers == decoder.num_layers:
             decoder_hidden = encoder_hidden
         else:
             decoder_hidden = torch.zeros(decoder.num_layers, 1, decoder.hidden_size)
         dp_output = []
+        gt_embeddings = []
+        l2_distances = []
+        l_loss = 0
         for l in range(target_number.int()):
             decoder_output, decoder_hidden = decoder(input=decoder_input, input_lengths=torch.tensor([1]), encoder_outputs=encoder_outputs, hidden=decoder_hidden) 
             dp_output.append(decoder_output.squeeze().detach().cpu().numpy().tolist())
+            gt_embeddings.append(target_tensor[iter_].squeeze().detach().cpu().numpy().tolist())
             decoder_input = decoder_output
+            criterion = nn.MSELoss()
+            #print(decoder_output[0].shape, target_tensor[iter_].shape)
+            loss = criterion(decoder_output.squeeze(), target_tensor[iter_].squeeze())
+            print(loss.item())
+            l2 = torch.norm(decoder_output.squeeze(), target_tensor.iter[iter_].squeeze())
+            print(l2.item())
+            l2_distances.append(l2.squeeze().detach().cpu().numpy().tolist())
+            l_loss += loss
+            print(decoder_output)
+            print(target_tensor[l].squeeze())
+            print(torch.norm(decoder_output-target_tensor[l,b],2).item())
+
+        print('final_loss:', (l_loss*50/target_number).item())
         print("number of embeddings in video:", len(dp_output))
-        output.append({'videoId':str(video_id), 'embeddings':dp_output})
+        #print(target_tensor[0,0])
+        output.append({'videoId':str(video_id), 'embeddings':dp_output, 'gt_embeddings': gt_embeddings, 'l2_distances': l2_distances, 'avg_l2_distance': l2_distances.sum()/len(l2_distances)})
     return output
 
 
 if __name__=="__main__":
 
     device='cuda'
-    checkpoint_path = '/home/eleonora/video_annotation/checkpoints/chkpt_batch3_lr0.001_enc1_dec1_tfratio1.0_wgDecay0.0_Adam.pt'
+    #checkpoint_path = '/home/eleonora/video_annotation/checkpoints/chkpt_batch3_lr0.001_enc1_dec1_tfratio1.0_wgDecay0.0_Adam.pt'
+    #checkpoint_path = '/home/louis/video_annotation/checkpoints/chkpt_batch3_lr0.001_enc1_dec1_tfratio1.0_wgDecay0.0_Adam.pt'
+    checkpoint_path = '../checkpoints/chkpt05-26_16:29:28.pt'
     rnge = [1,4]
     test_table = video_lookup_table_from_range(rnge[0],rnge[1])
 
@@ -114,6 +135,6 @@ if __name__=="__main__":
     """
     output = get_output_gen(checkpoint_path, h5_test_generator)
 
-    print(output)
+    #print(output)
     with open('output.txt', 'w') as outfile:
         json.dump(output, outfile)
