cc774853b0da552a62e7c4eeb63fb26028ff588b
Lou1sM <oneillml@tcd.ie>
2019-11-22 08:45:52 +0000

    full path to test file

--- src/test_gt.py
+++ src/test_gt.py
@@ -5,15 +5,14 @@ import argparse
 from pdb import set_trace
 from copy import deepcopy
 
-
-gt_json_fname = f'/data1/louis/data/rdf_video_captions/MSVD-wordnet-25d.json'
+gt_json_fname = f'/home/louis/test_new_msvd.json'
 with open(gt_json_fname) as f: gt_json_as_dict = {dp['video_id']: dp for dp in json.load(f)}
 
 outputs_json_fname = f'/data1/louis/data/rdf_video_captions/test_gt.json'
 with open(outputs_json_fname) as f: outputs = json.load(f)
 
 gt_outputs = []
-for dp in outputs[:10]:
+for dp in outputs:
     new = deepcopy(dp)
     new['embeddings'] = gt_json_as_dict[dp['video_id']]['gt_embeddings']
     gt_outputs.append(new)
@@ -21,7 +20,6 @@ for dp in outputs[:10]:
 sames = [dp['embeddings'] == gt_json_as_dict[dp['video_id']]['gt_embeddings'] for dp in gt_outputs]
 try: assert sum(sames) == len(sames)
 except: set_trace()
-set_trace()
 mlp_fname = f'/data1/louis/data/MSVD-wordnet-25d-mlps.pickle'
 mlp_dict = make_mlp_dict_from_pickle(mlp_fname,sigmoid=True)
 
