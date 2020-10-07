c7cdd125153633a7b4ccad1bfec0d302fb67d5f9
Lou1sM <oneillml@tcd.ie>
2020-10-07 13:46:06 +0100

    delete old script files

--- src/get_ranges.py
+++
@@ -1,2 +0,0 @@
-ranges = [(max([max([e[i] for e in dp['gt_embeddings']]) for dp in d]), min([min([e[i] for e in dp['gt_embeddings']]) for dp in d])) for i in range(25)]
-means = [sum([sum([e[i] for e in dp['gt_embeddings']])/(len(dp['gt_embeddings']*1970)) for dp in d]) for i in range(25)]
